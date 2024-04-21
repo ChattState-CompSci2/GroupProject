3973a07173faaae0107165ddd6c97946a9c4ad25
Weston <weston_hale@outlook.com>
2024-04-21 17:16:03 -0400

    Add coin change and a few products

--- src/Receipt.java
+++ src/Receipt.java
@@ -43,8 +43,7 @@ public class Receipt {
     /**
      *Formats receipt 
      */
-    @Override
-    public String toString() {
+    public String getReceipt(double cash) {
         String receiptString = String.format("Receipt: %s\n", this.getDateString());
         receiptString += "----------------------------------------\n";
         for (int i = 0; i < items.size(); i++) {
@@ -52,7 +51,10 @@ public class Receipt {
             receiptString += String.format("[%d] %-10s Price: $%.2f Tax: $%.2f\n", i, item.getProduct().getName(), item.getPrice(), item.getTax());
         }
         receiptString += "----------------------------------------\n";
-        receiptString += "Total: $" + String.format("%.2f", getTotal());
+        receiptString += "Total: $" + String.format("%.2f\n", getTotal());
+
+        receiptString += String.format("Tender: $%.2f\n", cash);
+        receiptString += String.format("Change: %s\n", getChangeString(getTotal(), cash));
         return receiptString;
     }
 
@@ -68,6 +70,84 @@ public class Receipt {
 
     public String getDateString(){
         return this.dateCreated.toString().replace(":", "-");
+    }
+
+    public String getChangeString (double owed, double payment) {
+        double change = payment - owed;
+        System.out.println("Subtotal: $" + owed + "\nAmount Payed: $" + payment + "\nChange: $" + change + "\n");
+        
+        String result = "";
+
+        if (change >= 50.00) {
+            int fifties = findCoin (change, 50.00);
+            change -= 50.00 * fifties;
+            result += ("\nFifties: " + fifties);
+
+        }
+        if (change >= 20.00) {
+            int twenties = findCoin (change, 20.00);
+            change -= 20.00 * twenties;
+            result += ("\nTwenties: " + twenties);
+
+        }
+        if (change >= 10.00) {
+            int tens = findCoin (change, 10.00);
+            change -= 10.00 * tens;
+            result += ("\nTens: " + tens);
+
+        }
+        if (change >= 5.00) {
+            int fives = findCoin (change, 5.00);
+            change -= 5.00 * fives;
+            result += ("\nFives: " + fives);
+
+        }
+        if (change >= 1.00) {
+            int ones = findCoin (change, 1.00);
+            change -= 1.00 * ones;
+            result += ("\nOnes: " + ones);
+            
+        }
+        if (change >= 0.25) {
+            int quarters = findCoin (change, 0.25);
+            change -= 0.25 * quarters;
+            result += ("\nQuarters: " + quarters);
+            
+        }
+        if (change >= 0.10) {
+            int dimes = findCoin (change, 0.10);
+            change -= 0.10 * dimes;
+            result += ("\nDimes: " + dimes);
+
+        }
+        if (change >= 0.05) {
+            int nickels = findCoin (change, 0.05);
+            change -= 0.05 * nickels;
+            result += ("\nNickels: " + nickels);
+
+        }
+        if (change > 0.00) {
+            int pennies = findCoin (change, 0.01);
+            change -= 0.01 * pennies;
+            result += ("\nPennies: " + pennies);
+
+        }
+        return result;
+    }
+    
+    public int findCoin (double change, double value) {
+        int finalCoin = 0;
+        double coin = change;
+        if (coin >= value) {
+            while (coin >= value) {
+                coin -= value;
+                finalCoin ++;
+            }
+            return finalCoin;
+        }
+        else {
+            return 0;
+        }
     }
 }
 
