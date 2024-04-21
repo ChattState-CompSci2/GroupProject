e588db300e8bd9ca1b7659c6006b3ec3e1d019b8
Trinity Henry <117396389+TrinityHenry@users.noreply.github.com>
2024-04-21 17:40:29 -0400

    Comments
    Trin

--- src/WeighableProduct.java
+++ src/WeighableProduct.java
@@ -1,13 +1,17 @@
-
+/**
+ * The WeighableProduct class enhances the Product class by providing functionality for calculating a product's price and tax based on its weight.
+* The total price is calculated by multiplying the product's price by its weight.
+*/
 
 class WeighableProduct extends Product {
-
-    // No default constructor for this class
-    
     public double getPrice(double weight) {
         return this.price * weight;
     }
-
+    
+/**
+* To compute total tax, multiply the product's tax rate by the total price multiplied by the weight.
+* The total price is the sum of the price plus the tax.
+*/
     public double getTax(double weight){
         return (this.price * this.tax_rate) * weight;
     }
